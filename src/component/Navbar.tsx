0c784d806ae0aa5d342050e53f2272536e2455bb
Jacky <jacky.teerapat@gmail.com>
2023-02-06 16:51:52 +0700

    setup-layout

---
+++ src/component/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Navbar() {
+    return (
+        <div className="md:w-full h-[3.6rem] border-2 border-rose-500">
+            navbar
+        </div>
+    );
+}
+
+export default Navbar;
